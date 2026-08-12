9d8bb61d16b7eb08388a89bc094fa3d3403120a9
Shreya Kumari <shreya200607@gmail.com>
2026-08-12 16:44:59 +0530

    Add eraser tool tooltip

--- FRONTEND/src/Components/whiteboard/Toolbar.jsx
+++ FRONTEND/src/Components/whiteboard/Toolbar.jsx
@@ -33,6 +33,7 @@ export default function Toolbar({
 </button>
       <button
         onClick={() => setTool("eraser")}
+        title="Erase Drawing"
         className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition"
       >
         🧽 Eraser
