8243b5571ce1bca36c57135e46561aa02e92c176
Anuvardhini T <anuvardhini2105@gmail.com>
2026-08-12 22:31:01 +0530

    Delete SyncSpace-RealTime-Collaboration-main directory

--- SyncSpace-RealTime-Collaboration-main/BACKEND/src/sockets/chatSocket.js
+++
@@ -1,9 +0,0 @@
-export default function chatSocket(io, socket) {
-  socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
-  });
-
-  socket.on("sendMessage", (data) => {
-    io.to(data.roomId).emit("receiveMessage", data);
-  });
-}
--- SyncSpace-RealTime-Collaboration-main/README.md
+++
@@ -1,1 +0,0 @@
-# SyncSpace-RealTime-Collaboration
